a060bf934b143c9fa542e12c484d9a9d62cb80a6
rbunce <richardallenbunce@gmail.com>
2017-04-25 08:07:13 -0700

    seperate autoloader from bootstrap

--- app/config/bootstrap.php
+++ app/config/bootstrap.php
@@ -5,15 +5,4 @@ if (!file_exists(__DIR__ . '/config.php')) {
 }
 
 require_once 'config.php';
-
-function autoloadPPRC($class)
-{
-    if (0 === strpos($class, 'PrintProviderReferenceClient_')) {
-        $path = dirname(__FILE__) . '/../../src/'.implode('/', explode('_', $class)) . '.php';
-        require_once $path;
-
-        return true;
-    }
-}
-
-spl_autoload_register('autoloadPPRC');
+require_once 'autoloader.php';
